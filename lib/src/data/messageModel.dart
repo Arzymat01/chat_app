e05fd20d41760c1db3310d94d3e239c1941f737e
Arzymat <104361795+Arzymat01@users.noreply.github.com>
2023-03-28 17:03:17 +0600

    Chattap finish

--- lib/src/data/messageModel.dart
+++ lib/src/data/messageModel.dart
@@ -1,15 +1,21 @@
 class Message {
-  const Message({
+  Message({
     required this.sender,
     required this.sms,
+    required this.dateTime,
+    this.isMy,
   });
   final String sender;
   final String sms;
+  final DateTime dateTime;
+  bool? isMy;
 
   Map<String, dynamic> toJson() {
     return <String, dynamic>{
       'sender': sender,
       'sms': sms,
+      'dateTime': dateTime.millisecondsSinceEpoch,
+      'isMy': isMy,
     };
   }
 
@@ -17,6 +23,8 @@ class Message {
     return Message(
       sender: map['sender'] as String,
       sms: map['sms'] as String,
+      dateTime: DateTime.fromMillisecondsSinceEpoch(map['dateTime'] as int),
+      isMy: map['isMy'] != null ? map['isMy'] as bool : null,
     );
   }
 }
--- lib/src/modules/home/service/home_service.dart
+++ lib/src/modules/home/service/home_service.dart
@@ -18,13 +18,20 @@ class HomeService {
     final sender = FirebaseAuth.instance.currentUser;
     if (sender != null) {
       final db = FirebaseFirestore.instance;
-      final Message message = Message(sender: sender.email!, sms: sms);
+      final Message message = Message(
+        sender: sender.email!,
+        sms: sms,
+        dateTime: DateTime.now(),
+      );
       await db.collection('mesages').add(message.toJson());
     }
   }
 
   static Stream<QuerySnapshot<Map<String, dynamic>>> streamMessage() {
     final db = FirebaseFirestore.instance;
-    return db.collection('mesages').snapshots();
+    return db
+        .collection('mesages')
+        .orderBy('dateTime', descending: true)
+        .snapshots();
   }
 }
